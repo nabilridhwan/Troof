5633ddf2b4d0175d4b2632231cdd932a5e27a61e
Nabil Ridhwan <nabridhwan@gmail.com>
2022-12-08 17:07:08 +0800

    feat: :sparkles: add tenor api to packages

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,5 +3,19 @@ import createRoom from "./createRoom";
 import getPlayer from "./getPlayer";
 import getRoom from "./getRoom";
 import joinRoom from "./joinRoom";
+import getFeaturedCategories from "./tenor/getFeaturedCategories";
+import getFeaturedGifs from "./tenor/getFeaturedGifs";
+import searchForGifs from "./tenor/searchForGifs";
+import tenorAxiosInstance from "./tenorAxiosInstance";
 
-export { axiosInstance, createRoom, getRoom, getPlayer, joinRoom };
+export {
+	axiosInstance,
+	createRoom,
+	getRoom,
+	getPlayer,
+	joinRoom,
+	tenorAxiosInstance,
+	getFeaturedCategories,
+	searchForGifs,
+	getFeaturedGifs,
+};
