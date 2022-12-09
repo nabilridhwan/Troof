90cd02ef6b773a4b07976c168d356375a798aa0c
Nabil Ridhwan <nabridhwan@gmail.com>
2022-12-09 13:59:58 +0800

    feat: :sparkles: updated file path for truth and dares

--- packages/helpers/src/files/truthOrDareGenerator.ts
+++ packages/helpers/src/files/truthOrDareGenerator.ts
@@ -1,8 +1,8 @@
 // https://github.com/ark-maker-bot/better-tord/blob/main/index.js
 
 // Read file from truth or dare generator
-import all_dares from "@troof/truth-or-dare/output/all_dares.json";
-import all_truths from "@troof/truth-or-dare/output/all_truths.json";
+import all_dares from "@troof/truth-or-dare/output/all_dare.json";
+import all_truths from "@troof/truth-or-dare/output/all_truth.json";
 
 import { randomInRange } from "make-random";
 
@@ -10,12 +10,12 @@ export async function get_truth() {
 	console.log("Getting truth...");
 	const randomNum = await randomInRange(0, all_truths.length);
 	console.log(`[TRUTH] Random number: ${randomNum}`);
-	return all_truths[randomNum];
+	return all_truths[randomNum].data;
 }
 export async function get_dare() {
 	console.log("Getting dare...");
 	const randomNum = await randomInRange(0, all_dares.length);
 
 	console.log(`[DARE] Random number: ${randomNum}`);
-	return all_dares[randomNum];
+	return all_dares[randomNum].data;
 }
