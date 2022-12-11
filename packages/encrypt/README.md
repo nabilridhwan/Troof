b40f967d5bf7849865b95ce9359a3b442cc5a14c
Nabil Ridhwan <nabridhwan@gmail.com>
2022-12-11 14:39:57 +0800

    feat: :sparkles: add encrypt package

---
+++ packages/encrypt/README.md
@@ -0,0 +1,9 @@
+# @troof/encrypt
+
+This package contains the encryption and decryption libraries for end-to-end encryption of text message data.
+
+### Encryption model
+
+The picture below explains everything about how the messages are encrypted
+
+![Encryption model](../../docs/encryption_model.png)
---
+++ packages/encrypt/index.ts
@@ -0,0 +1,54 @@
+import NodeRSA from "node-rsa";
+
+export interface GeneratedKeyPair {
+	private: string;
+	public: string;
+}
+
+export namespace Encryption {
+	const key = new NodeRSA();
+
+	export async function generateKeyPair(): Promise<GeneratedKeyPair> {
+		const k = key.generateKeyPair();
+
+		const priv = k.exportKey("private");
+		const pub = k.exportKey("public");
+
+		return {
+			private: priv,
+			public: pub,
+		};
+	}
+
+	export function getPrivateKey(k: GeneratedKeyPair) {
+		return k.private;
+	}
+
+	export function getPublicKey(k: GeneratedKeyPair) {
+		return k.public;
+	}
+
+	export function encryptWithPublic(data: string, publicKey: string) {
+		console.log("encryptWithPublic", data, publicKey);
+		return new NodeRSA().importKey(publicKey, "public").encrypt(data, "base64");
+	}
+
+	export function encryptWithPrivate(data: string, privateKey: string) {
+		console.log("encryptWithPrivate", data, privateKey);
+		return new NodeRSA()
+			.importKey(privateKey, "private")
+			.encryptPrivate(data, "base64");
+	}
+
+	export function decryptWithPublic(data: string, publicKey: string) {
+		console.log("decryptWithPublic", data, publicKey);
+		return new NodeRSA()
+			.importKey(publicKey, "public")
+			.decryptPublic(data, "utf8");
+	}
+
+	export function decryptWithPrivate(data: string, privateKey: string) {
+		console.log("decryptWithPrivate", data, privateKey);
+		return new NodeRSA().importKey(privateKey, "private").decrypt(data, "utf8");
+	}
+}
